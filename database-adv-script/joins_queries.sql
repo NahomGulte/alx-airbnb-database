d0dd6e6cc2d74ae6a28a8836dc76c92c3f3b2d1d
Nahom.Gulte <nahomg@awash.local>
2025-05-17 10:29:34 +0300

    commit for the DB tasks

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,18 +1,18 @@
 ----the inner join of the user table and the booking table
 SELECT customer.name, listing_id.
 FROM booking
-INNER JOIN Users ON Users.Customer_id = booking.Customer_id;
+FULL OUTER JOIN Users ON Users.Customer_id = booking.Customer_id;
 
 ----the right join of the booking table and the payment table
 
 SELECT customer.name, status,transaction_ref.
 FROM payment
-INNER JOIN Booking ON Booking.booking_id = payment.Customer_id;
+RIGHT JOIN Booking ON Booking.booking_id = payment.Customer_id;
 
 ----the Left join of the user table and the Listing table
 SELECT title, Discription.
 FROM booking
-INNER JOIN Users ON Users.user_id = Listing.Created_by;
+LEFT JOIN Users ON Users.user_id = Listing.Created_by;
 
 
 
