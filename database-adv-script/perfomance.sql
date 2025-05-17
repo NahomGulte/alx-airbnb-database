4bc3bfbe1adb2b18b178b801ae2cea7d42f38a0c
Nahom.Gulte <nahomg@awash.local>
2025-05-17 10:57:21 +0300

    commit for the DB tasks

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -1,11 +1,1 @@
--- Step 1: Initial Query
-SELECT *
-FROM 
-    booking 
-JOIN 
-    users ON booking.customer_id = user.id
-JOIN 
-    listing  ON booking.listing_id = listing.id
-JOIN 
-    payments  ON booking.id = payment.booking_id;
-
+SELECT * FROM booking WHERE status ='closed' and Payment='complete'
